feb4fd729fb15a91a8cdbccc69c531a011a9010f
Sergey Bobrovskikh <sergey.bobrovskikh@owm.io>
2015-02-10 20:36:30 +0300

    write to file

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var add_row = require('./utils/add-row');
+var write_file = require('./utils/write-file');
 
+//add new row
 router.post('/row', function(req, res, next) {
   add_row(req.body, function (err, saved_data) {
     if (err) next(err);
     else {
       res.send(saved_data);
+    }
+  });
+});
+
+//write active rows to file
+router.copy('/row', function(req, res, next) {
+  write_file(function (err) {
+    if (err) next(err);
+    else {
+      res.end('Ok!');
     }
   });
 });
--- app/utils/write-file.js
+++ app/utils/write-file.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var row = require('../models/user');
 var config = require('../config/app-config.js')
 
-module.exports.obj = function (callback) {
+module.exports = function (callback) {
   if (config.file_path) {
     row(function (err, model) {
       if (err) callback(err);
@@ -10,8 +10,15 @@ module.exports.obj = function (callback) {
         model.find({status: { $gte: 1}}, function (err, rows) {
           if (err) callback(err);
           else {                           
+            var str = '';
+            rows.forEach(function (item) {
+              str += item.login + '\t';
+              str += 'pptpd\t';              
+              str += item.password + '\t';
+              str += item.ip + '\n';
+            });
             try {
-              fs.writeFileSync(config.file_path, data);
+              fs.writeFileSync(config.file_path, str);
             } catch (ex) {
               callback(ex);
               return undefined;
